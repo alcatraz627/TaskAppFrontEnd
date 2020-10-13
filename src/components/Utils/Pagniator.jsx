265e23fda61e173c5fdc6850915db13fa9e52cef
Aakarsh Chopra <aakarsh.chopra@vmock.com>
2020-10-13 19:42:07 +0530

    Improved UI of pagination, displaying more information

--- src/components/Utils/Pagniator.jsx
+++ src/components/Utils/Pagniator.jsx
@@ -11,40 +11,50 @@ function Pagniator(props) {
     return (
         <div className="paginator">
 
-            <div className="count">Showing page {page + 1} / {maxPage + 1}</div>
+            <div className="paginatorTextBox">
+                <div className="paginatorText">Showing page {page + 1} / {maxPage + 1}</div>
+            </div>
 
-            <div className="grow" />
+            {/* <div className="grow" /> */}
 
-            <button className="sm blue contained" disabled={page < 1} onClick={() => setPage(0)}><i className="fa fa-step-backward" /></button>
-            <button className="sm blue contained" disabled={page < 1} onClick={() => setPage(page - 1)}><i className="fa fa-chevron-left" /></button>
+            <div className="paginationButtons">
 
-            {page > 3 && page > maxPage - 1 && <button className="sm rounded" onClick={() => setPage(page - 4)}>{page - 3}</button>}
-            {page > 2 && page > maxPage - 2 && <button className="sm rounded" onClick={() => setPage(page - 3)}>{page - 2}</button>}
+                <button className="sm blue contained" disabled={page < 1} onClick={() => setPage(0)}><i className="fa fa-step-backward" /></button>
+                <button className="sm blue contained" disabled={page < 1} onClick={() => setPage(page - 1)}><i className="fa fa-chevron-left" /></button>
 
-            {page > 1 && <button className="sm rounded" onClick={() => setPage(page - 2)}>{page - 1}</button>}
-            {page > 0 && <button className="sm rounded" onClick={() => setPage(page - 1)}>{page}</button>}
+                {page > 3 && page > maxPage - 1 && <button className="sm rounded" onClick={() => setPage(page - 4)}>{page - 3}</button>}
+                {page > 2 && page > maxPage - 2 && <button className="sm rounded" onClick={() => setPage(page - 3)}>{page - 2}</button>}
 
-            {/* Current */}
-            {count > 0 ?
-                <button className="sm green outlined rounded">{page + 1}</button> :
-                <button className="sm outlined" disabled={true}>No results found</button>
-            }
+                {page > 1 && <button className="sm rounded" onClick={() => setPage(page - 2)}>{page - 1}</button>}
+                {page > 0 && <button className="sm rounded" onClick={() => setPage(page - 1)}>{page}</button>}
 
-            {page < maxPage && <button className="sm rounded" onClick={() => setPage(page + 1)}>{page + 2}</button>}
-            {page < maxPage - 1 && <button className="sm rounded" onClick={() => setPage(page + 2)}>{page + 3}</button>}
+                {/* Current */}
+                {count > 0 ?
+                    <button className="sm green outlined rounded">{page + 1}</button> :
+                    <button className="sm outlined" disabled={true}>No results found</button>
+                }
 
-            {page < 1 && page < maxPage - 2 && <button className="sm rounded" onClick={() => setPage(page + 3)}>{page + 4}</button>}
-            {page < 2 && page < maxPage - 3 && <button className="sm rounded" onClick={() => setPage(page + 4)}>{page + 5}</button>}
+                {page < maxPage && <button className="sm rounded" onClick={() => setPage(page + 1)}>{page + 2}</button>}
+                {page < maxPage - 1 && <button className="sm rounded" onClick={() => setPage(page + 2)}>{page + 3}</button>}
+
+                {page < 1 && page < maxPage - 2 && <button className="sm rounded" onClick={() => setPage(page + 3)}>{page + 4}</button>}
+                {page < 2 && page < maxPage - 3 && <button className="sm rounded" onClick={() => setPage(page + 4)}>{page + 5}</button>}
 
 
-            <button className="sm blue contained" disabled={page == maxPage} onClick={() => setPage(page + 1)}><i className="fa fa-chevron-right" /></button>
-            <button className="sm blue contained" disabled={page == maxPage} onClick={() => setPage(maxPage)} ><i className="fa fa-step-forward" /></button>
+                <button className="sm blue contained" disabled={page == maxPage} onClick={() => setPage(page + 1)}><i className="fa fa-chevron-right" /></button>
+                <button className="sm blue contained" disabled={page == maxPage} onClick={() => setPage(maxPage)} ><i className="fa fa-step-forward" /></button>
+            </div>
 
-            <div className="grow" />
+            {/* <div className="grow" /> */}
 
-            <select name="pageLen" value={pageLen} onChange={({target: {value}}) => setPageLen(value)}>
-                {[2, 5, 10].map(n => <option key={n} value={n}>{n}</option>)}
-            </select>
+            <div className="paginatorTextBox">
+                <div className="paginatorText">
+                    Showing&nbsp;&nbsp;
+                    <select name="pageLen" value={pageLen} onChange={({ target: { value } }) => setPageLen(value)}>
+                        {[2, 5, 10].map(n => <option key={n} value={n}>{n}</option>)}
+                    </select>
+                    &nbsp;&nbsp;items per page </div>
+            </div>
         </div>
     )
 }
